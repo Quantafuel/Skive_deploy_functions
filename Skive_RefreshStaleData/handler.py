cf3c16937e0e4fcd64a9996de4a0a0ec97c3f64c
henrikbreivik <henrik.breivik@quantafuel.com>
2025-02-14 14:08:47 +0100

    RefreshStaleData13

--- Skive_RefreshStaleData/handler.py
+++ Skive_RefreshStaleData/handler.py
@@ -22,6 +22,7 @@ def handle(client):
         "2s=P02_EKG_QN613:M_POS",
         "2s=P02_EKG_BF604:M_MID",
         "2s=P03_QJD_BF603:M_MID",
+        "2s=P03_EKG_QN613:M_POS",
     ]
 
     minutes_ago = 10
