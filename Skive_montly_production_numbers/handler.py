d5315c83a4cbeeaf20c08112a5ba30f4e5626e29
Espen Nordsveen <espen.nordsveen@quantafuel.com>
2025-02-27 16:07:44 +0100

    Deploying Montly Status with schedule through CI/CD pipeline

---
+++ Skive_montly_production_numbers/handler.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 23 09:18:58 2022
+
+@author: EspenUlsetNordsveen
+"""
+
+
+def handle(client):
+    from datetime import datetime, timedelta
+
+    import pytz
+
+    from dateutil.relativedelta import relativedelta
+
+    data = {
+        "start_time": "1d-ago",
+        "end_time": "now",
+        "productionhours1": ["2s=L1_PRODUCTION_HOURS:Q_TT"],
+        "productionhours2": ["2s=L2_PRODUCTION_HOURS:Q_TT"],
+        "productionhours3": ["2s=L3_PRODUCTION_HOURS:Q_TT"],
+        "productionhours4": ["2s=L4_PRODUCTION_HOURS:Q_TT"],
+    }
+    today = datetime.today()
+    t = today.replace(day=1, hour=0, minute=0, second=0)
+    t = t - timedelta(days=1)
+    time_now = t.replace(hour=23, minute=59, second=59)
+
+    dk_tz = pytz.timezone("Europe/Copenhagen")
+    time_local = dk_tz.localize(time_now)
+
+    df_dps_24_1 = (
+        client.time_series.data.retrieve_latest(
+            external_id=data["productionhours1"], before=time_local - relativedelta(months=1)
+        )
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_now_1 = (
+        client.time_series.data.retrieve_latest(external_id=data["productionhours1"], before=time_local)
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_24_2 = (
+        client.time_series.data.retrieve_latest(
+            external_id=data["productionhours2"], before=time_local - relativedelta(months=1)
+        )
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_now_2 = (
+        client.time_series.data.retrieve_latest(external_id=data["productionhours2"], before=time_local)
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_24_3 = (
+        client.time_series.data.retrieve_latest(
+            external_id=data["productionhours3"], before=time_local - relativedelta(months=1)
+        )
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_now_3 = (
+        client.time_series.data.retrieve_latest(external_id=data["productionhours3"], before=time_local)
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_24_4 = (
+        client.time_series.data.retrieve_latest(
+            external_id=data["productionhours4"], before=time_local - relativedelta(months=1)
+        )
+        .to_pandas()
+        .iloc[0, 0]
+    )
+    df_dps_now_4 = (
+        client.time_series.data.retrieve_latest(external_id=data["productionhours4"], before=time_local)
+        .to_pandas()
+        .iloc[0, 0]
+    )
+
+    operating_last24_all = [
+        df_dps_now_1 - df_dps_24_1,
+        df_dps_now_2 - df_dps_24_2,
+        df_dps_now_3 - df_dps_24_3,
+        df_dps_now_4 - df_dps_24_4,
+    ]
+
+    client.time_series.data.insert([(time_local, operating_last24_all[0])], external_id="montly_production_hours_line1")
+    client.time_series.data.insert([(time_local, operating_last24_all[1])], external_id="montly_production_hours_line2")
+    client.time_series.data.insert([(time_local, operating_last24_all[2])], external_id="montly_production_hours_line3")
+    client.time_series.data.insert([(time_local, operating_last24_all[3])], external_id="montly_production_hours_line4")
+
+    return operating_last24_all
