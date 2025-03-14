8835718a99fbe9760061a2af5b0de8b774425e40
henrikbreivik <henrik.breivik@quantafuel.com>
2025-03-14 16:45:51 +0100

    Function monitoring initial commit7

--- Skive_Function_Monitoring/handler.py
+++ Skive_Function_Monitoring/handler.py
@@ -70,7 +70,7 @@ def handle(client, data):
         # ExtPipe = client.extraction_pipelines.retrieve(external_id=extractionPipelineExtId)
 
         results = []
-        results.append(6)
+        results.append(7)
 
         # Timestamps
         now = datetime.now()
