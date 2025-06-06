8e683fdc659108fe72199f7ffc7c853236eeb0de
Quantafuel-dev <96174475+Quantafuel-dev@users.noreply.github.com>
2025-06-06 13:18:44 +0200

    Skive downtime (#138)
    * Downtime register tag lookup to raw
    
    * Downtime register tag lookup to raw
    
    * Downtime register tag lookup to raw
    
    * Added comment
    
    * Changed common lib placement outside of function
    
    * Cleaning

--- Skive_downtime_tag_lookup/handler.py
+++ Skive_downtime_tag_lookup/handler.py
@@ -7,15 +7,17 @@ Created on Tue May 13 12:39:38 2025
 
 # from cog_client import client
 
+from common.utilities import Sharepoint
+
 
 def handle(secrets, client):
-    from common.utilities import Sharepoint
 
     CLIENT_ID = secrets.get("lists-id")
     CLIENT_SECRET = secrets.get("lists-secret")
 
     sharepoint = Sharepoint("S-Skive470", CLIENT_ID, CLIENT_SECRET, client)
 
+    # get list ID
     lists_id = sharepoint.get_lists_id()
 
     # Get tag data from sharepoint
