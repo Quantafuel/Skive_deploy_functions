37e838d78ff3609274c9a9833aacc4205524f703
Espen Nordsveen <espen.nordsveen@quantafuel.com>
2025-12-11 11:00:03 +0100

    Deploying hot gas filter runtime calculations

--- Skive_hot_gas_filter_runtime_calculations/handler.py
+++ Skive_hot_gas_filter_runtime_calculations/handler.py
@@ -14,6 +14,7 @@ def handle(client):
 
     lines = [1, 2, 3, 4]
     clusters = [1, 2, 3, 4, 5, 6]
+    # Fill runtimes per cluster
     for line in lines:
         cluster_run_times = []
         for cluster in clusters:
