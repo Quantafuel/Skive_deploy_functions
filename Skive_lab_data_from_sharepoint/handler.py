62faf441cfdfa3aff950b6ab874d1b33a5c9b1d3
Espen Nordsveen <espen.nordsveen@quantafuel.com>
2026-04-20 13:45:43 +0200

    Renewal of sharepoint secret

--- Skive_lab_data_from_sharepoint/handler.py
+++ Skive_lab_data_from_sharepoint/handler.py
@@ -13,7 +13,6 @@ def handle(secrets, client):
     CLIENT_ID = secrets.get("lists-id")
     CLIENT_SECRET = secrets.get("lists-secret")
 
-    # Sharepoint list class
     class MSListData:
         """
         A class to represent Sharepoint list data in Skive. The class fetches data from Sharepoint lists in the Viridor domain
